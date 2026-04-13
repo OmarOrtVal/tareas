e7eb7441316569caa805aa1b58bd596e4026365d
OmarOrtVal <23308060610674@cetis61.edu.mx>
2026-04-13 08:58:45 -0600

    clase

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Proyecto iniciado")
+import flet as ft 
+from src.controllers.UsuariosController import AuthController
+from src.controllers.TareaController import TareaController
+from src.views.LoginView import LoginView
+from src.views dashboard import DashboardView
 
-
-if __name__ == "__main__":
-    main()
+def main(page: ft.Page):
+    auth_ctrl=AuthController()
+    task_ctrl=TareaController()
+    
+    def route_change(route):
+        page.view.clear()
+        if
