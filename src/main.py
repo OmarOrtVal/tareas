f72da3454e40e5f7cb68e57ce6355172edb43aca
OmarOrtVal <2330860610674@cetis61.edu.mx>
2026-04-13 10:49:31 -0600

    clase 2

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import flet as ft 
-from src.controllers.UsuariosController import AuthController
-from src.controllers.TareaController import TareaController
-from src.views.LoginView import LoginView
-from src.views dashboard import DashboardView
+from controllers.UsuariosController import AuthController
+from controllers.TareaController import TareaController
+from views.LoginView import LoginView
+from views.dashboard import DashboardView
 
 def main(page: ft.Page):
     auth_ctrl=AuthController()
@@ -10,4 +10,18 @@ def main(page: ft.Page):
     
     def route_change(route):
         page.view.clear()
-        if
+        if page.route == "/":
+            page.views.append(LoginView(page,auth_ctrl))
+        elif page.route =="/dashboard":
+            page.views.append(DashboardView(page,task_ctrl))
+        page.update()
+        
+    page.on_route_change=route_change
+    page.go("/")
+    
+    def star(page: ft.Page):
+        def route_change(e):
+        page.views.append(DashboardView())
+    
+if __name__ =="__main__":
+    ft.run(main)
