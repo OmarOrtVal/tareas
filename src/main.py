8a02fc72a4765b9720dfd43038c199bc8967a243
OmarOrtVal <23308060610674@cetis61.edu.mx>
2026-04-14 07:25:25 -0600

    clase

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import flet as ft
-from controllers.UsuariosController import AuthController
+from controllers.UsuariosController import AuthController 
 from controllers.TareaController import TareaController
+from views.LoginView import LoginView
 from views.DashboardView import DashboardView  
-from views.LoginView import LoginView          
 
 def start(page: ft.Page):
     auth_ctrl = AuthController()
@@ -10,17 +10,18 @@ def start(page: ft.Page):
 
     def route_change(e):
         page.views.clear()
-        
+
         if page.route == "/":
             page.add(ft.Text("Caso 1"))
             page.views.append(LoginView(page, auth_ctrl))
         elif page.route == "/dashboard":
             page.views.append(DashboardView(page, task_ctrl))
+        
         if not page.views:
             page.views.append(
                 ft.View("/", [ft.Text("Error: Ruta no encontrada o vista vacía")])
             )
-            
+
         page.update()
 
     page.on_route_change = route_change
